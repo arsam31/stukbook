f4dc2076520ed4ba96db7db323db82628bef2aae
arsam <arsamrahmaan31@gmail.com>
2017-10-05 10:12:11 -0400

    added friendship status detection

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,14 @@
 module UsersHelper
+  def action_buttons(user)
+    case current_user.friendship_status(user) when "friends"
+                                                "remove friendship"
+      when "pending"
+        "Cancel Request"
+      when "requested"
+        "Accept or Deny"
+      when "not_friends"
+        "Add As Friend"
+    end
+  end
+
 end
