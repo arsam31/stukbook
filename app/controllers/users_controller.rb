900759ad05ee0eac6a06d979b266396676c69729
arsam <arsamrahmaan31@gmail.com>
2017-10-09 04:20:40 -0400

    Friendship actions now possible

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show]
+  before_action :authenticate_user!
 
   def index
     @users = User.all
@@ -7,6 +8,8 @@ class UsersController < ApplicationController
 
   def show
   end
+
+  
 
   private
 
